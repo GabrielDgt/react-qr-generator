9db5299663b6cf80387762d71f55f4552f04e1bc
GabrielDgt <gaboz.03@gmail.com>
2023-02-09 01:45:24 +0100

    modified logic and semantic tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-import { useRef, useState } from 'react'
+import { useState } from 'react'
 import '../style.css'
 import Image from './components/Image'
 
 export function App () {
-  const refAux = useRef(null)
   const [text, setText] = useState(null)
 
+  const handleChange = (event) => {
+    const newText = event.target.value
+    if (newText.startsWith(' ')) return
+    setText(newText)
+  }
+
   const handleClick = () => {
-    const { current } = refAux
-    setText(current.value)
+    setText(null)
+    document.getElementById('text').value = ''
   }
 
   return (
-    <main>
+    <div className='page'>
       <form>
         <label htmlFor='text'>Insert text to QR</label>
-        <input type='text' id='text' placeholder='Write here...' ref={refAux} />
+        <input type='text' id='text' placeholder='Write here...' onChange={handleChange} />
       </form>
-      <button onClick={handleClick}>Generate QR</button>
       <Image text={text} />
-    </main>
+      {text && <button onClick={handleClick}>Clear</button>}
+    </div>
   )
 }
